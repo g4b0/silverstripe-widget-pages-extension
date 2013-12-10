d7f96a35290b76af7a9933ce0ccbc692fa8842ee
g4b0 <gabriele.brosulo@gmail.com>
2013-12-10 16:19:08 +0100

    From has_many to many_many

--- code/WidgetPage.php
+++ code/WidgetPage.php
@@ -1,37 +1,51 @@
 <?php
 
 /**
-* Widget Page
-*/
+ * Widget Page
+ */
 class WidgetPage extends DataExtension {
 
-	private static $has_many = array(
-		'Widgets' => 'Widget.WidgetPage'
+//	private static $has_many = array(
+//		'Widgets' => 'Widget.WidgetPage'
+//	);
+	private static $many_many = array(
+			'Widgets' => 'Widget'
 	);
-	
+	private static $many_many_extraFields = array(
+			'Widgets' => array(
+					'PageSort' => 'Int'
+			)
+	);
+
 	function updateCMSFields(\FieldList $fields) {
 		parent::updateCMSFields($fields);
-		
+
 		$adder = new GridFieldAddNewMultiClass();
-
-		if(is_array($this->owner->config()->get("allowed_widgets"))){
-			$adder->setClasses($this->owner->config()->get("allowed_widgets"));
-		}
-
-		$config = GridFieldConfig_RecordEditor::create()
-			->removeComponentsByType("GridFieldAddNewButton")
-			->addComponent($adder)
-			->addComponent(new GridFieldOrderableRows());
-
-		$fields->addFieldToTab("Root.Main",
-			GridField::create('Widgets','Widgets',$this->owner->Widgets())
-				->setConfig($config)
-			,"Content"
-		);
-		$fields->removeByName("Content");
 		
+		// Allowed classes
+		$allowed = $this->owner->config()->get("allowed_widgets");
+		if (is_array($allowed)) {
+			// Filter classes for creating new ones
+			$adder->setClasses($allowed);
+		}
+		
+		$config = GridFieldConfig_RelationEditor::create()
+						->removeComponentsByType("GridFieldAddNewButton")
+						->addComponent($adder)
+						->addComponent(new GridFieldOrderableRows('PageSort'));
+		
+		// It's not possible to add existing through GridFieldAddExistingSearchButton since
+		// it display by default $Title in its template and we want to search by $WidgetName
+		$config->getComponentByType('GridFieldAddExistingAutocompleter')->setSearchFields(array('WidgetName'))->setResultsFormat('$WidgetName');
+
+		$gridField = GridField::create('Widgets', 'Widgets', $this->owner->Widgets())->setConfig($config);
+		$fields->addFieldToTab("Root.Main", $gridField, "Content");
+		
+		$fields->removeByName("Content");
+	}
+	
+	public function SortedWidgets() {
+		return $this->owner->Widgets()->sort('PageSort');
 	}
 
 }
-
-
--- code/WidgetPageWidget.php
+++ code/WidgetPageWidget.php
@@ -1,9 +1,30 @@
 <?php
 
 class WidgetPageWidget extends DataExtension {
-
-	private static $has_one = array(
+	
+	private static $db = array(
+			'WidgetName' => 'Varchar'
+	);
+//	private static $has_one = array(
+//			'WidgetPage' => 'WidgetPage'
+//	);
+	private static $belongs_many_many = array(
 			'WidgetPage' => 'WidgetPage'
 	);
+	private static $summary_fields = array(
+			'WidgetName'
+	);
+	
+	public function updateCMSFields(\FieldList $fields) {
+		parent::updateCMSFields($fields);
+		
+		$field = new TextField('WidgetName', 'Widget Name');
+		$fields->add($field);		
+	}
+	
+	public function Title() {
+		//return $this->owner->WidgetName;
+		return "BBB";
+	}
 
 }
