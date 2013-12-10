1c8a3888188eb5d23c44c0b144f27e9165413675
g4b0 <gabriele.brosulo@gmail.com>
2013-12-10 16:21:00 +0100

    code cleanup

--- code/WidgetPage.php
+++ code/WidgetPage.php
@@ -1,13 +1,10 @@
 <?php
 
 /**
- * Widget Page
+ * Widget Page extension
  */
 class WidgetPage extends DataExtension {
 
-//	private static $has_many = array(
-//		'Widgets' => 'Widget.WidgetPage'
-//	);
 	private static $many_many = array(
 			'Widgets' => 'Widget'
 	);
@@ -21,29 +18,29 @@ class WidgetPage extends DataExtension {
 		parent::updateCMSFields($fields);
 
 		$adder = new GridFieldAddNewMultiClass();
-		
+
 		// Allowed classes
 		$allowed = $this->owner->config()->get("allowed_widgets");
 		if (is_array($allowed)) {
 			// Filter classes for creating new ones
 			$adder->setClasses($allowed);
 		}
-		
+
 		$config = GridFieldConfig_RelationEditor::create()
 						->removeComponentsByType("GridFieldAddNewButton")
 						->addComponent($adder)
 						->addComponent(new GridFieldOrderableRows('PageSort'));
-		
+
 		// It's not possible to add existing through GridFieldAddExistingSearchButton since
 		// it display by default $Title in its template and we want to search by $WidgetName
 		$config->getComponentByType('GridFieldAddExistingAutocompleter')->setSearchFields(array('WidgetName'))->setResultsFormat('$WidgetName');
 
 		$gridField = GridField::create('Widgets', 'Widgets', $this->owner->Widgets())->setConfig($config);
 		$fields->addFieldToTab("Root.Main", $gridField, "Content");
-		
+
 		$fields->removeByName("Content");
 	}
-	
+
 	public function SortedWidgets() {
 		return $this->owner->Widgets()->sort('PageSort');
 	}
--- code/WidgetPageWidget.php
+++ code/WidgetPageWidget.php
@@ -1,30 +1,25 @@
 <?php
 
+/**
+ * Widget extension
+ */
 class WidgetPageWidget extends DataExtension {
-	
+
 	private static $db = array(
 			'WidgetName' => 'Varchar'
 	);
-//	private static $has_one = array(
-//			'WidgetPage' => 'WidgetPage'
-//	);
 	private static $belongs_many_many = array(
 			'WidgetPage' => 'WidgetPage'
 	);
 	private static $summary_fields = array(
 			'WidgetName'
 	);
-	
+
 	public function updateCMSFields(\FieldList $fields) {
 		parent::updateCMSFields($fields);
-		
+
 		$field = new TextField('WidgetName', 'Widget Name');
-		$fields->add($field);		
-	}
-	
-	public function Title() {
-		//return $this->owner->WidgetName;
-		return "BBB";
+		$fields->add($field);
 	}
 
 }
