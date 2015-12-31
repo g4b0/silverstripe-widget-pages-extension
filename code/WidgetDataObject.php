f77380e2ca63a1ace4ec50f4818ca22bf0ce5adc
helpfulrobot <chris@silverstripe.com>
2016-01-01 05:40:48 +1300

    Converted to PSR-2

--- code/WidgetDataObject.php
+++ code/WidgetDataObject.php
@@ -3,84 +3,87 @@
 /**
  * Extension for DataObjects that permit them to have an arbitrary number of WidgetAreas
  */
-class WidgetDataObject extends DataExtension {
+class WidgetDataObject extends DataExtension
+{
 
-	private static $has_one = array();
+    private static $has_one = array();
 
-	public function updateCMSFields(\FieldList $fields) {
-		parent::updateCMSFields($fields);
+    public function updateCMSFields(\FieldList $fields)
+    {
+        parent::updateCMSFields($fields);
 
-		// Loop over each WidgetArea
-		$has_one = $this->owner->has_one();
-		foreach ($has_one as $name => $class) {
-			if ($class == 'WidgetArea') {
+        // Loop over each WidgetArea
+        $has_one = $this->owner->has_one();
+        foreach ($has_one as $name => $class) {
+            if ($class == 'WidgetArea') {
 
-				/*
-				 * Widget gridfield 
-				 */
-				$adder = new GridFieldAddNewMultiClass();
+                /*
+                 * Widget gridfield 
+                 */
+                $adder = new GridFieldAddNewMultiClass();
 
-				// Allowed classes
-				$allowed = $this->owner->config()->get("allowed_widgets");
-				if (is_array($allowed)) {
-					// Filter classes for creating new ones
-					$adder->setClasses($allowed);
-				}
+                // Allowed classes
+                $allowed = $this->owner->config()->get("allowed_widgets");
+                if (is_array($allowed)) {
+                    // Filter classes for creating new ones
+                    $adder->setClasses($allowed);
+                }
 
-				$config = GridFieldConfig_RelationEditor::create()
-								->removeComponentsByType("GridFieldAddNewButton")
-								->addComponent($adder)
-								->addComponent(new GridFieldOrderableRows('WidgetAreaSort'));
+                $config = GridFieldConfig_RelationEditor::create()
+                                ->removeComponentsByType("GridFieldAddNewButton")
+                                ->addComponent($adder)
+                                ->addComponent(new GridFieldOrderableRows('WidgetAreaSort'));
 
-				// It's not possible to add existing through GridFieldAddExistingSearchButton since
-				// it display by default $Title in its template and we want to search by $WidgetName
-				$config->getComponentByType('GridFieldAddExistingAutocompleter')->setSearchFields(array('WidgetName'))->setResultsFormat('$WidgetName');
+                // It's not possible to add existing through GridFieldAddExistingSearchButton since
+                // it display by default $Title in its template and we want to search by $WidgetName
+                $config->getComponentByType('GridFieldAddExistingAutocompleter')->setSearchFields(array('WidgetName'))->setResultsFormat('$WidgetName');
 
-				// WidgetArea's Widget GridField
-				$gridField = GridField::create($name, $name, $this->owner->$name()->SortedWidgets(false))->setConfig($config);
-				$fields->addFieldToTab("Root.$name", $gridField);
-			}
-		}
-	}
+                // WidgetArea's Widget GridField
+                $gridField = GridField::create($name, $name, $this->owner->$name()->SortedWidgets(false))->setConfig($config);
+                $fields->addFieldToTab("Root.$name", $gridField);
+            }
+        }
+    }
 
-	/**
-	 * Creates WidgetArea DataObjects in not aleready done.
-	 */
-	public function onAfterWrite() {
-		parent::onAfterWrite();
+    /**
+     * Creates WidgetArea DataObjects in not aleready done.
+     */
+    public function onAfterWrite()
+    {
+        parent::onAfterWrite();
 
-		$has_one = $this->owner->has_one();
-		// Loop over each WidgetArea
-		foreach ($has_one as $name => $class) {
-			if ($class == 'WidgetArea') {
-				// Create the WidgetArea if it not exist
-				$dbName = $name . 'ID';
-				// Can't use $this->owner->$name()->ID since it doesn't
-				// work with DataObjects, it works just with Pages. SS bug?
-				if ($this->owner->$dbName == 0) {
-					$wa = new WidgetArea();
-					$wa->write();
-					$this->owner->$dbName = $wa->ID;
-					if ($this->owner->hasExtension('Versioned')) {
-						$this->owner->writeWithoutVersion();
-					} else {
-						$dbg = $this->owner->$name();
-						$this->owner->write();
-					}
-				}
-			}
-		}
-	}
+        $has_one = $this->owner->has_one();
+        // Loop over each WidgetArea
+        foreach ($has_one as $name => $class) {
+            if ($class == 'WidgetArea') {
+                // Create the WidgetArea if it not exist
+                $dbName = $name . 'ID';
+                // Can't use $this->owner->$name()->ID since it doesn't
+                // work with DataObjects, it works just with Pages. SS bug?
+                if ($this->owner->$dbName == 0) {
+                    $wa = new WidgetArea();
+                    $wa->write();
+                    $this->owner->$dbName = $wa->ID;
+                    if ($this->owner->hasExtension('Versioned')) {
+                        $this->owner->writeWithoutVersion();
+                    } else {
+                        $dbg = $this->owner->$name();
+                        $this->owner->write();
+                    }
+                }
+            }
+        }
+    }
 
-	public function WidgetArea($name = '') {
-		$retVal = null;
+    public function WidgetArea($name = '')
+    {
+        $retVal = null;
 
-		$has_one = $this->owner->has_one();
-		if (isset($has_one[$name])) {
-			$retVal = $this->owner->$name()->SortedWidgets();
-		}
+        $has_one = $this->owner->has_one();
+        if (isset($has_one[$name])) {
+            $retVal = $this->owner->$name()->SortedWidgets();
+        }
 
-		return $retVal;
-	}
-
+        return $retVal;
+    }
 }
--- code/WidgetPage.php
+++ code/WidgetPage.php
@@ -3,54 +3,57 @@
 /**
  * Extension for Page that permit them to have an arbitrary number of WidgetAreas
  */
-class WidgetPage extends WidgetDataObject {
+class WidgetPage extends WidgetDataObject
+{
 
-	private static $db = array(
-			'InheritSideBar' => 'Boolean',
-	);
-	private static $defaults = array(
-			'InheritSideBar' => true
-	);	
+    private static $db = array(
+            'InheritSideBar' => 'Boolean',
+    );
+    private static $defaults = array(
+            'InheritSideBar' => true
+    );
 
-	public function updateCMSFields(\FieldList $fields) {
+    public function updateCMSFields(\FieldList $fields)
+    {
 
-		// Inherit
-		$fields->addFieldToTab(
-						"Root.Main", new CheckboxField("InheritSideBar", 'Inherit Sidebar From Parent')
-		);
+        // Inherit
+        $fields->addFieldToTab(
+                        "Root.Main", new CheckboxField("InheritSideBar", 'Inherit Sidebar From Parent')
+        );
 
-		// Check if this page needs its own WidgetArea
-		if ($this->owner->InheritSideBar == false) {
-			parent::updateCMSFields($fields);
-		}
-	}
+        // Check if this page needs its own WidgetArea
+        if ($this->owner->InheritSideBar == false) {
+            parent::updateCMSFields($fields);
+        }
+    }
 
-	/**
-	 * Creates WidgetArea DataObjects in not aleready done.
-	 */
-	public function onAfterWrite() {
+    /**
+     * Creates WidgetArea DataObjects in not aleready done.
+     */
+    public function onAfterWrite()
+    {
 
-		// Check if this page needs its own WidgetArea
-		if ($this->owner->InheritSideBar == false) {
-			parent::onAfterWrite();
-		}
-	}
-	
-	
-	public function WidgetArea($name='') {
-		$retVal = null;
-		
-		// Check if this page needs its own WidgetArea
-		if ($this->owner->InheritSideBar == true) {
-			// Inherit the WidgetArea from its Parent
-			if ($this->owner->ParentID > 0 && $this->owner->getParent()->hasExtension('WidgetPage')) {
-				$retVal = $this->owner->getParent()->WidgetArea($name);
-			}
-		} else {
-			$retVal = parent::WidgetArea($name);
-		}
-		
-		return $retVal;
-	}
-
+        // Check if this page needs its own WidgetArea
+        if ($this->owner->InheritSideBar == false) {
+            parent::onAfterWrite();
+        }
+    }
+    
+    
+    public function WidgetArea($name='')
+    {
+        $retVal = null;
+        
+        // Check if this page needs its own WidgetArea
+        if ($this->owner->InheritSideBar == true) {
+            // Inherit the WidgetArea from its Parent
+            if ($this->owner->ParentID > 0 && $this->owner->getParent()->hasExtension('WidgetPage')) {
+                $retVal = $this->owner->getParent()->WidgetArea($name);
+            }
+        } else {
+            $retVal = parent::WidgetArea($name);
+        }
+        
+        return $retVal;
+    }
 }
